67803ff2e11057229fa045b261b48c7eb47f92f6
JoseManuel1906 <159016395+JoseManuel1906@users.noreply.github.com>
2024-02-07 12:50:39 +0100

    Add files via upload

---
+++ practicaGestionDatosR.R
@@ -0,0 +1,25 @@
+Sys.which("make")
+
+1
+edades_descubrimientos <- c(set.seed(145))
+2
+cantidad_artefactos <- c(17)
+3
+profundidad_hallazgos <- c(25 'metros')
+4
+materiales_encontrados <- c(5)
+5
+años_excavados <- c(4)
+6
+matrix(1)
+colnames<- ('sitios arqueologicos') <- c('Valencina, Merida, Italica, Dolmen de Lacara')
+rownames<- ('años de las excavaciones') <- c(1989, 1992, 1998, 2005)
+7
+matrix(2)
+colnames<- ('descubrimientos edades') <- c('calcolítico, edad clásica, edad clásica, edad del hierro')
+rownames<- ('regiones excavadas') <- c('Andalucía, Extremadura, Andalucía, Extremadura')
+8
+matrix(3)
+colnames<- ('periodos') <- c('calcolítico, edad clásica, edad clásica, edad del hierro')
+rownames<- ('tipos') <- c(5, 6, 1, 5)
+
